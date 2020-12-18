5f854e4cee0619fc779966a136c9dcf7f4bfd4ae
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-12-18 20:54:25 +0200

    Add SearchController

--- JSONiTUNES/ViewController.swift
+++ JSONiTUNES/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var table: UITableView!
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupSearchBar()
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        table.delegate = self
+        table.dataSource = self
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "123"
+        return cell
+    }
+
+}
+
+extension ViewController: UISearchBarDelegate {
+    
+}
